38f66a8ee2645d10e8a7818cbdfdd0c5931ea5dc
quq99 <eequq99@gmail.com>
2016-04-10 23:14:56 +0800

    slip the dataset

--- chapter3-DecisionTree/trees.py
+++ chapter3-DecisionTree/trees.py
@@ -10,7 +10,7 @@ def calcShannonEnt(dataSet):
 	for featVec in dataSet:
 		currentLabel = featVec[-1]
 		if currentLabel not in labelCounts.keys():
-			labelCounts[currentLabel] = 0
+			labelCounts[currentLabel] = 0#add new and set the value to 0
 		labelCounts[currentLabel] += 1
 	shannonEnt = 0.0
 	for key in labelCounts:
@@ -29,4 +29,15 @@ def createDataSet():
 	labels = ['no surfacing', 'flippers']
 	return dataSet, labels
 
-	
+
+#splitDataSet
+
+def splitDataSet(dataSet, axis, value):
+	#axis:features; value:feature need to return
+	retDataSet = []
+	for featVec in dataSet:
+		if featVec[axis] == value:
+			reducedFeatVec = featVec[:axis]
+			reducedFeatVec.extend(featVec[axis+1:])
+			retDataSet.append(reducedFeatVec)
+	return retDataSet
