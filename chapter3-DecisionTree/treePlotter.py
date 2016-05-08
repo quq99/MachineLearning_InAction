2266c9d850f8ab709f253cd7c9b9f9257c99d1d7
quq99 <eequq99@gmail.com>
2016-05-08 22:57:51 +0800

    add function to calculate leafNode and treeDepth

--- chapter3-DecisionTree/treePlotter.py
+++ chapter3-DecisionTree/treePlotter.py
@@ -10,6 +10,34 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
 			textcoords='axes fraction', va="center",\
 			ha="center", bbox=nodeType, arrowprops=arrow_args)
 
+
+def getNumLeafs(myTree):
+	numLeafs = 0
+	firstStr = myTree.keys()[0]
+	secondDict = myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__ == 'dict':
+			numLeafs += getNumLeafs(secondDict[key])
+		else:
+			numLeafs += 1
+	return numLeafs
+
+def getTreeDepth(myTree):
+	maxDepth = 0
+	firstStr = myTree.keys()[0]
+	secondDict = myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__ == 'dict':
+			thisDepth = 1 + getTreeDepth(secondDict[key])
+		else:
+			thisDepth = 1
+		if thisDepth > maxDepth:
+			maxDepth = thisDepth
+	return maxDepth
+
+
+
+#an example
 def createPlot():
 	fig = plt.figure(1, facecolor='white')
 	fig.clf()
