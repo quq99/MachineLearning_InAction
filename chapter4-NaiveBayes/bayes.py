fix the underflow when probabilities multiplies by change it to logarithm

chapter4-NaiveBayes/bayes.py
@@ -47,8 +47,8 @@ def trainNB0(trainMatrix, trainCategory):
 		else:
 			p0Num += trainMatrix[i]
 			p0Denom += sum(trainMatrix[i])
-	p1Vect = p1Num/p1Denom
-	p0Vect = p0Num/p0Denom
+	p1Vect = np.log(p1Num/p1Denom)
+	p0Vect = np.log(p0Num/p0Denom)
 	return p0Vect, p1Vect, pAbusive
 #main
 listOposts, listClasses = loadDataSet()