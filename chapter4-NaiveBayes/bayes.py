9ddea72a46fe8e7765f799abd7b7029ed120f18f
quq99 <eequq99@gmail.com>
2016-05-27 12:26:18 +0800

    add trainNB0 function. calculate p(wi|c0) p(wi|c1) p(c1)

--- chapter4-NaiveBayes/bayes.py
+++ chapter4-NaiveBayes/bayes.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def loadDataSet():
 	postingList = [['my', 'dog', 'has', 'flea', \
 			'problems', 'help','please'],\
@@ -29,7 +32,24 @@ def setOfWords2Vec(vocabList, inputSet):
 			print "the word: %s is not in the vocabulary!" % word
 	return returnVec
 
-
+def trainNB0(trainMatrix, trainCategory):
+	numTrainDocs = len(trainMatrix)
+	numwords = len(trainMatrix[0])
+	pAbusive = sum(trainCategory)/float(numTrainDocs)#p(c1)
+	p0Num = np.zeros(numwords)
+	p1Num = np.zeros(numwords)
+	p0Denom = 0.0
+	p1Denom = 0.0
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+	p1Vect = p1Num/p1Denom
+	p0Vect = p0Num/p0Denom
+	return p0Vect, p1Vect, pAbusive
 #main
 listOposts, listClasses = loadDataSet()
 myvocablist = createVocabList(listOposts)
@@ -38,3 +58,13 @@ returnvec = setOfWords2Vec(myvocablist, listOposts[0])
 print returnvec
 returnvec2 = setOfWords2Vec(myvocablist, listOposts[3])
 print returnvec2
+
+trainMat = []
+for p in listOposts:
+	trainMat.append(setOfWords2Vec(myvocablist, p))
+
+p0V,p1V,pAb = trainNB0(trainMat, listClasses)
+print p0V
+print p1V
+print pAb
+
